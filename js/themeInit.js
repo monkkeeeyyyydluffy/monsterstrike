b68a2b43387f0fbe9199933b0d2b40cbf3d68f63
Alex Kuang <uoroaix@gmail.com>
2025-04-17 19:40:48 -0700

    fix calculator overflow on phone

---
+++ js/themeInit.js
@@ -0,0 +1,12 @@
+// js/themeInit.js
+;(function () {
+  if (
+    localStorage.theme === 'dark' ||
+    (!('theme' in localStorage) &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches)
+  ) {
+    document.documentElement.classList.add('dark')
+  } else {
+    document.documentElement.classList.remove('dark')
+  }
+})()
